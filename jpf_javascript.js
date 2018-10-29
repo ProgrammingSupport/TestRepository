cc0eb045a61dd6a22023840933dc5d6bf799263e
BInsertName <43142212+BInsertName@users.noreply.github.com>
2018-10-29 08:23:27 -0500

    What I got

---
+++ jpf_javascript.js
@@ -0,0 +1,29 @@
+//check answer
+
+//var x = document.getElementById("00");
+//var y = x.getElementsByTagName("*")[0];
+document.getElementById("check").onclick = function()
+ {
+	var answer = document.getElementById("00").getElementsByTagName("*")[0].value;
+
+	alert(answer);
+ }
+/* 	var answer = 1;
+	alert(answer)
+	document.getElementById('00').value;
+	if (answer == 1)
+	{
+		alert('correct');
+	}
+	else
+	{
+		alert('incorrect');
+	} */
+
+
+
+
+
+//create game
+
+//document.getElementById('00').innerHTML = '1'; // This should be my method to insert html
